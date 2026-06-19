e6281fe6d5275f3c85af0c20a2ce93b021f531e7
00d3vi1 <00d3vi1@gmail.com>
2026-06-18 21:40:00 -0700

    Update UnoTest.java

--- UnoTest.java
+++ UnoTest.java
@@ -1,17 +1,17 @@
 import java.util.Arrays;
+import java.io.*;
 
 public class UnoTest {
 
 	public static void main(String[] args) {
-		// deck
-		// last card played
-		// draw card. 
+ 
+		final String ANSI_YELLOW = "\u001B[33m";
+		final String ANSI_GREEN = "\u001B[32m";
+		final String ANSI_RED = "\u001B[31m";
 		
-		System.out.println("Uno Tester");
+		Player.setColor(ANSI_GREEN);
 		
-		String[] deck1 = {"green_1", "green_2", "red_3", "yellow_8"};
-		String[] deckFull = {"red_2", "yellow_2", "red_8", "blue_10", "green_1"};
-		String[] deckUno = { "black_5", "empty", "empty", "empty", "empty" };
+		System.out.println(ANSI_GREEN + "Uno Tester");
 		
 		
 		UnoCard cDeck = new UnoCard();
@@ -22,25 +22,18 @@ public class UnoTest {
 		
 		Player player1 = new Player();
 		
-//		Player player2 = new Player();
-//		player2.playerInit(5, deckFull);
-//		
-//		Player player3 = new Player();
-//		player3.playerInit(5, deckUno);
 		
-		
-		
-		System.out.println("Start Draw array " + Arrays.toString(startDraw));
+		Player.setColor(ANSI_RED);
+		System.out.println(ANSI_YELLOW + "Start Draw array " + Arrays.toString(startDraw));
 		player1.playerInit(10, startDraw);
+
+		String discardPile = "yellow_1";
+		String drawCard = "green_5";
 		
-		player1.playerTurn("green_5");
+		player1.playerTurn(discardPile, drawCard);
 		
-		
-		
-		
-		//String cardPicker = pickCard(unoDeck);
-		//System.out.println("Show me string: " + cardPicker);
-		//System.out.println(player1.toString());
+
+		System.out.println("NEXT PLAYER TURN!");
 		
 		
 	}
